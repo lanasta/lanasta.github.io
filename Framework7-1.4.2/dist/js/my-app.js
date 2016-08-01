e13586324ac7c794f6cbfe91a3a964af2d2d8af8
Anastasia Lalamentik <anastasia@Anastasias-MacBook-Pro.local>
2016-08-01 17:23:32 -0400

    try

--- Framework7-1.4.2/dist/js/my-app.js
+++ Framework7-1.4.2/dist/js/my-app.js
@@ -3,6 +3,8 @@ var myApp = new Framework7({
   materialRipple: true,
   materialRippleElements: '.ripple, a.link, a.item-link, .button, .modal-button, .tab-link, .label-radio, .label-checkbox, .actions-modal-button, a.searchbar-clear, .floating-button',
   animatePages: true,
+  pushState: true,
+  swipePanel: 'right'
 });
 
 // Export selectors engine
